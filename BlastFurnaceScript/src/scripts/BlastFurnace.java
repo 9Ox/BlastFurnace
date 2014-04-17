699541925e9ac473bae92809050c67b57501567f
Nolan <9oxsteelflux@gmail.com>
2014-04-17 19:09:02 -0400

    Create BlastFurnace class

---
+++ BlastFurnaceScript/src/scripts/BlastFurnace.java
@@ -0,0 +1,21 @@
+package scripts;
+
+import java.awt.Graphics;
+import org.tribot.script.Script;
+import org.tribot.script.interfaces.Painting;
+
+/**
+ * @author erickho123, starfox
+ */
+public class BlastFurnace 
+        extends Script
+        implements Painting {
+
+    @Override
+    public void run() {
+    }
+
+    @Override
+    public void onPaint(Graphics g1) {
+    }
+}
