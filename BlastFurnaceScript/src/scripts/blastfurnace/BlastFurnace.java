02eb1f7d3c6444f5877c2f29e2ec63f5fa158a43
erickho123 <erickho123@gmail.com>
2014-04-18 09:03:13 -0500

    Functionality to shoutcaller name, use argument : to get shoutcaller name

--- BlastFurnaceScript/src/scripts/blastfurnace/BlastFurnace.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/BlastFurnace.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import org.tribot.api2007.util.ThreadSettings;
 import org.tribot.script.Script;
 import org.tribot.script.interfaces.Arguments;
+import org.tribot.script.interfaces.MessageListening07;
 import org.tribot.script.interfaces.Painting;
 
 import scripts.blastfurnace.framework.JobLoop;
@@ -14,13 +15,15 @@ import scripts.blastfurnace.jobs.CashMoney;
 import scripts.blastfurnace.jobs.Fueler;
 import scripts.blastfurnace.jobs.Pedaler;
 import scripts.blastfurnace.jobs.PipeRepairer;
+import scripts.blastfurnace.jobs.Pumper;
+import scripts.blastfurnace.jobs.ShoutCaller;
 
 /**
  * @author erickho123, starfox
  */
 public class BlastFurnace 
 extends Script
-implements Painting, Arguments {
+implements Painting, Arguments, MessageListening07 {
 
 	/**
 	 * Creates a new BlastFurnace object.
@@ -50,6 +53,7 @@ implements Painting, Arguments {
 				JobManager.addJob(new Fueler());
 				break;
 			case "Pedaler":
+				Pumper.shoutCallerName = input.split(":")[1];
 				JobManager.addJob(new Pedaler());
 				break;
 			case "PipeRepairer":
@@ -58,10 +62,50 @@ implements Painting, Arguments {
 			case "CashMoney":
 				JobManager.addJob(new CashMoney());
 				break;
+			case "ShoutCaller":
+				JobManager.addJob(new ShoutCaller());
+				break;
 			default:
 				break;
 			}
-			
+
 		}
+	}
+
+	@Override
+	public void clanMessageReceived(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void personalMessageReceived(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playerMessageReceived(String arg0, String arg1) {
+		if(!Pumper.shoutCallerName.isEmpty() && Pumper.shoutCallerName.equalsIgnoreCase(arg0)) {
+			if(arg1.contains("STOP NIGGA")) {
+				Pumper.startPumping = false;
+			} else {
+				Pumper.startPumping = true;
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void serverMessageReceived(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tradeRequestReceived(String arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
