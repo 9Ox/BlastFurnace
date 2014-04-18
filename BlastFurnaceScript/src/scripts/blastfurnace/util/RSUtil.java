ae9195727173c2f044c07fe30d47a6e495fc7e28
erickho123 <erickho123@gmail.com>
2014-04-18 04:55:05 -0500

    Addtion : Methods added clickRSNPC, clickRSObject

--- BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
@@ -1,5 +1,36 @@
 package scripts.blastfurnace.util;
 
-public class Empty {
+import org.tribot.api.Clicking;
+import org.tribot.api.interfaces.Clickable;
+import org.tribot.api2007.Game;
+import org.tribot.api2007.Player;
+import org.tribot.api2007.types.RSNPC;
+import org.tribot.api2007.types.RSObject;
+import org.tribot.api2007.types.RSTile;
 
+public class RSUtil {
+
+	/**
+	 * @param entity RSObject you desire to click
+	 * @return returns true if it successfully clicks the object.
+	 */
+	public static boolean clickRSObject(final RSObject entity) {
+		if(Clicking.click(entity)) {
+			final RSTile destination = Game.getDestination();
+			return destination != null && Player.getPosition().distanceTo(destination) <= 1;
+		}
+		return false;
+	}
+	
+	/**
+	 * @param entity RSNPC you desire to click
+	 * @return returns true if it successfully clicks the npc.
+	 */
+	public static boolean clickRSNPC(final RSNPC entity) {
+		if(Clicking.click(entity)) {
+			final RSTile destination = Game.getDestination();
+			return destination != null && Player.getPosition().distanceTo(destination) <= 1;
+		}
+		return false;
+	}
 }
