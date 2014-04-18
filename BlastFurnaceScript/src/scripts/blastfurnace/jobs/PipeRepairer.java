06d6d842fadf48eefad033aa49cb13f4fe1c7bc9
Nolan <9oxsteelflux@gmail.com>
2014-04-18 06:36:15 -0400

    Finished pipe repairer class

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
@@ -1,7 +1,15 @@
 package scripts.blastfurnace.jobs;
 
+import org.tribot.api.Timing;
+import org.tribot.api.types.generic.Condition;
+import org.tribot.api2007.Camera;
 import org.tribot.api2007.Objects;
+import org.tribot.api2007.Player;
+import org.tribot.api2007.types.RSObject;
 import scripts.blastfurnace.framework.Job;
+import scripts.blastfurnace.util.Get;
+import scripts.blastfurnace.util.RSUtil;
+import scripts.blastfurnace.util.Walking;
 
 /**
  * @author Starfox, erickho123
@@ -17,6 +25,24 @@ public class PipeRepairer extends Job {
 
     @Override
     public void doJob() {
-	
+	RSObject pipe = Get.get(40, BROKEN_PIPE_IDS);
+        if (pipe != null) {
+            if (pipe.isOnScreen() && Player.getAnimation() == -1) {
+                if (RSUtil.clickRSObject("Repair Pipes", pipe)) {
+                    Timing.waitCondition(new Condition() {
+                        @Override
+                        public boolean active() {
+                            return Player.getAnimation() != -1;
+                        }
+                    }, 4000);
+                }
+            } else {
+                if (pipe.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+                    Camera.turnToTile(pipe);
+                } else {
+                    Walking.walkTo(pipe.getPosition());
+                }
+            }
+        }
     }
 }
