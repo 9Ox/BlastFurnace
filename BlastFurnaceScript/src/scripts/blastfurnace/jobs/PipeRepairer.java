ead4e35a8cadf95838d3d4677da7ac9d04477d24
Nolan <9oxsteelflux@gmail.com>
2014-04-18 06:55:50 -0400

    Changed PipeRepairer class for actions not ids

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
@@ -6,6 +6,8 @@ import org.tribot.api2007.Camera;
 import org.tribot.api2007.Objects;
 import org.tribot.api2007.Player;
 import org.tribot.api2007.types.RSObject;
+import org.tribot.api2007.types.RSObjectDefinition;
+import org.tribot.api2007.types.RSTile;
 import scripts.blastfurnace.framework.Job;
 import scripts.blastfurnace.util.Get;
 import scripts.blastfurnace.util.RSUtil;
@@ -16,19 +18,51 @@ import scripts.blastfurnace.util.Walking;
  */
 public class PipeRepairer extends Job {
 
-    private final int[] BROKEN_PIPE_IDS = {9121};
+    private final String REPAIR_OPTION = "Repair Pipes";
     
     @Override
     public boolean shouldDo() {
-        return Objects.findNearest(40, BROKEN_PIPE_IDS).length > 0;
+        RSObject[] objects = Objects.findNearest(40, "Pipes");
+        for (RSObject object : objects) {
+            if (object != null) {
+                RSObjectDefinition def = object.getDefinition();
+                if (def != null) {
+                    String[] actions = def.getActions();
+                    if (actions != null) {
+                        for (String s : actions) {
+                            if (s.equalsIgnoreCase(REPAIR_OPTION)) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     @Override
     public void doJob() {
-	RSObject pipe = Get.get(40, BROKEN_PIPE_IDS);
+	RSObject pipe = null;
+        RSObject[] objects = Objects.findNearest(40, "Pipes");
+        for (RSObject object : objects) {
+            if (object != null) {
+                RSObjectDefinition def = object.getDefinition();
+                if (def != null) {
+                    String[] actions = def.getActions();
+                    if (actions != null) {
+                        for (String s : actions) {
+                            if (s.equalsIgnoreCase(REPAIR_OPTION)) {
+                                pipe = object;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         if (pipe != null) {
             if (pipe.isOnScreen() && Player.getAnimation() == -1) {
-                if (RSUtil.clickRSObject("Repair Pipes", pipe)) {
+                if (RSUtil.clickRSObject(REPAIR_OPTION, pipe)) {
                     Timing.waitCondition(new Condition() {
                         @Override
                         public boolean active() {
