ac8bbc2c59bcd62161414edc6d82e68d113e5e56
Nolan <9oxsteelflux@gmail.com>
2014-04-17 20:03:32 -0400

    Create JobLoader class

---
+++ BlastFurnaceScript/src/scripts/framework/JobLoader.java
@@ -0,0 +1,14 @@
+package scripts.framework;
+
+/**
+ * @author Starfox
+ */
+public class JobLoader {
+    
+    /**
+     * Should be used to load jobs into the JobManager anonymously.
+     * This should only be used if more than one Job is being added to the JobManager.
+     */
+    public static void loadJobs() {
+    }
+}
