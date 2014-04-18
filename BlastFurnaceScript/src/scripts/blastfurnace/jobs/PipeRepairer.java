eb8c9cf6904906590ae01339bbc2bc057a990203
erickho123 <erickho123@gmail.com>
2014-04-18 06:03:20 -0500

    Shortened code using the Filters class
    Used the filters class to shorten down the code.

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
@@ -5,9 +5,11 @@ import org.tribot.api.types.generic.Condition;
 import org.tribot.api2007.Camera;
 import org.tribot.api2007.Objects;
 import org.tribot.api2007.Player;
+import org.tribot.api2007.ext.Filters;
 import org.tribot.api2007.types.RSObject;
 import org.tribot.api2007.types.RSObjectDefinition;
 import org.tribot.api2007.types.RSTile;
+
 import scripts.blastfurnace.framework.Job;
 import scripts.blastfurnace.util.Get;
 import scripts.blastfurnace.util.RSUtil;
@@ -18,65 +20,38 @@ import scripts.blastfurnace.util.Walking;
  */
 public class PipeRepairer extends Job {
 
-    private final String REPAIR_OPTION = "Repair Pipes";
-    
-    @Override
-    public boolean shouldDo() {
-        RSObject[] objects = Objects.findNearest(40, "Pipes");
-        for (RSObject object : objects) {
-            if (object != null) {
-                RSObjectDefinition def = object.getDefinition();
-                if (def != null) {
-                    String[] actions = def.getActions();
-                    if (actions != null) {
-                        for (String s : actions) {
-                            if (s.equalsIgnoreCase(REPAIR_OPTION)) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
+	private final String REPAIR_OPTION = "Repair Pipes";
 
-    @Override
-    public void doJob() {
-	RSObject pipe = null;
-        RSObject[] objects = Objects.findNearest(40, "Pipes");
-        for (RSObject object : objects) {
-            if (object != null) {
-                RSObjectDefinition def = object.getDefinition();
-                if (def != null) {
-                    String[] actions = def.getActions();
-                    if (actions != null) {
-                        for (String s : actions) {
-                            if (s.equalsIgnoreCase(REPAIR_OPTION)) {
-                                pipe = object;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if (pipe != null) {
-            if (pipe.isOnScreen() && Player.getAnimation() == -1) {
-                if (RSUtil.clickRSObject(REPAIR_OPTION, pipe)) {
-                    Timing.waitCondition(new Condition() {
-                        @Override
-                        public boolean active() {
-                            return Player.getAnimation() != -1;
-                        }
-                    }, 4000);
-                }
-            } else {
-                if (pipe.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
-                    Camera.turnToTile(pipe);
-                } else {
-                    Walking.walkTo(pipe.getPosition());
-                }
-            }
-        }
-    }
+	@Override
+	public boolean shouldDo() {
+		RSObject[] pipes = Objects.findNearest(40, Filters.Objects.actionsEquals(REPAIR_OPTION));
+		return pipes.length > 0;
+	}
+
+	@Override
+	public void doJob() {
+		RSObject pipe = null;
+		RSObject[] pipes = Objects.findNearest(40, Filters.Objects.actionsEquals(REPAIR_OPTION));
+		if(pipes.length > 0) {
+			pipe = pipes[0];
+			if (pipe != null) {
+				if (pipe.isOnScreen() && Player.getAnimation() == -1) {
+					if (RSUtil.clickRSObject(REPAIR_OPTION, pipe)) {
+						Timing.waitCondition(new Condition() {
+							@Override
+							public boolean active() {
+								return Player.getAnimation() != -1;
+							}
+						}, 4000);
+					}
+				} else {
+					if (pipe.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+						Camera.turnToTile(pipe);
+					} else {
+						Walking.walkTo(pipe.getPosition());
+					}
+				}
+			}
+		}
+	}
 }
