926dd8c86d2c318ce3c4b9479f39c812b9cc2557
Nolan <9oxsteelflux@gmail.com>
2014-04-17 20:03:49 -0400

    Create JobManager class

---
+++ BlastFurnaceScript/src/scripts/framework/JobManager.java
@@ -0,0 +1,53 @@
+package scripts.framework;
+
+import java.util.ArrayList;
+import org.tribot.api.types.generic.Condition;
+
+/**
+ * @author Starfox
+ */
+public class JobManager {
+    
+    private static final ArrayList<Job> jobs = new ArrayList<>();
+    private static final ArrayList<Condition> terminateConditions = new ArrayList<>();
+    
+    /**
+     * Adds a Job to the JobManager.
+     * @param job The Job to add.
+     */
+    public static void addJob(Job job) {
+        jobs.add(job);
+    }
+    
+    /**
+     * Adds a terminate condition to the JobManager.
+     * @param c The condition to add.
+     */
+    public static void addTerminateCondition(Condition c) {
+        terminateConditions.add(c);
+    }
+    
+    /**
+     * Goes through all Jobs added to the JobManager and runs the first Job that should be done, then returns if a Job was done.
+     */
+    public static void runJobs() {
+        for (Job job : jobs) {
+            if (job.shouldDo()) {
+                job.doJob(); return;
+            }
+        }
+    }
+    
+    /**
+     * Checks whether or not the script should terminate. If any condition is met, the script will terminate.
+     * @return true if any of the terminate conditions are met; false otherwise.
+     */
+    public static boolean shouldTerminate() {
+        for (Condition c : terminateConditions) {
+            if (c.active()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
