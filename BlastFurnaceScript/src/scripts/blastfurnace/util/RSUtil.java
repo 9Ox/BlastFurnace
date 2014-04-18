34ad7d761835439add990fb9143a8f22130d6f9e
erickho123 <erickho123@gmail.com>
2014-04-18 06:32:22 -0500

    Added method: clickRSGroundItem

--- BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
@@ -3,6 +3,7 @@ package scripts.blastfurnace.util;
 import org.tribot.api.Clicking;
 import org.tribot.api2007.Game;
 import org.tribot.api2007.Player;
+import org.tribot.api2007.types.RSGroundItem;
 import org.tribot.api2007.types.RSNPC;
 import org.tribot.api2007.types.RSObject;
 import org.tribot.api2007.types.RSTile;
@@ -30,6 +31,20 @@ public class RSUtil {
      * @return returns true if it successfully clicks the npc.
      */
     public static boolean clickRSNPC(final String option, final RSNPC... entities) {
+        if (Clicking.click(option, entities)) {
+            final RSTile destination = Game.getDestination();
+            return destination != null && Player.getPosition().distanceTo(destination) <= 1;
+        }
+        return false;
+    }
+    
+    /**
+     * Clicks the RSGroundItem specified with the passed in option
+     * @param option The option to select.
+     * @param entities RSGroundItem(s) you want to click.
+     * @return returns true if it successfully clicks the RSGroundItem.
+     */
+    public static boolean clickRSGroundItem(final String option, final RSGroundItem... entities) {
         if (Clicking.click(option, entities)) {
             final RSTile destination = Game.getDestination();
             return destination != null && Player.getPosition().distanceTo(destination) <= 1;
