2dc0ab79301e51c17415d4e1081fc45988baa6e7
Nolan <9oxsteelflux@gmail.com>
2014-04-18 06:03:29 -0400

    Update clicking methods to add option param

--- BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
@@ -1,7 +1,6 @@
 package scripts.blastfurnace.util;
 
 import org.tribot.api.Clicking;
-import org.tribot.api.interfaces.Clickable;
 import org.tribot.api2007.Game;
 import org.tribot.api2007.Player;
 import org.tribot.api2007.types.RSNPC;
@@ -10,27 +9,31 @@ import org.tribot.api2007.types.RSTile;
 
 public class RSUtil {
 
-	/**
-	 * @param entity RSObject you desire to click
-	 * @return returns true if it successfully clicks the object.
-	 */
-	public static boolean clickRSObject(final RSObject entity) {
-		if(Clicking.click(entity)) {
-			final RSTile destination = Game.getDestination();
-			return destination != null && Player.getPosition().distanceTo(destination) <= 1;
-		}
-		return false;
-	}
-	
-	/**
-	 * @param entity RSNPC you desire to click
-	 * @return returns true if it successfully clicks the npc.
-	 */
-	public static boolean clickRSNPC(final RSNPC entity) {
-		if(Clicking.click(entity)) {
-			final RSTile destination = Game.getDestination();
-			return destination != null && Player.getPosition().distanceTo(destination) <= 1;
-		}
-		return false;
-	}
+    /**
+     * Clicks the RSObject specified with the passed in option.
+     * @param option The option to select.
+     * @param entities RSObject(s) you want to click.
+     * @return returns true if it successfully clicks the object.
+     */
+    public static boolean clickRSObject(final String option, final RSObject... entities) {
+        if (Clicking.click(option, entities)) {
+            final RSTile destination = Game.getDestination();
+            return destination != null && Player.getPosition().distanceTo(destination) <= 1;
+        }
+        return false;
+    }
+
+    /**
+     * Clicks the RSNPC specified with the passed in option
+     * @param option The option to select.
+     * @param entities RSNPC(s) you want to click.
+     * @return returns true if it successfully clicks the npc.
+     */
+    public static boolean clickRSNPC(final String option, final RSNPC... entities) {
+        if (Clicking.click(entities)) {
+            final RSTile destination = Game.getDestination();
+            return destination != null && Player.getPosition().distanceTo(destination) <= 1;
+        }
+        return false;
+    }
 }
