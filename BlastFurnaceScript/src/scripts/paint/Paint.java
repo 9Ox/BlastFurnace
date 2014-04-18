b754f74f000701a5558675372d95ed50e9d2e9c3
Nolan <9oxsteelflux@gmail.com>
2014-04-17 21:47:44 -0400

    Addition getImageFromUrl() method

--- BlastFurnaceScript/src/scripts/paint/Paint.java
+++ BlastFurnaceScript/src/scripts/paint/Paint.java
@@ -1,8 +1,27 @@
 package scripts.paint;
 
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import org.tribot.api.General;
+
 /**
  * @author Starfox
  */
 public class Paint {
-
+    /**
+     * Gets the image from the specified url and returns it as an Image object.
+     *
+     * @param url The url to get the image from.
+     * @return The image retrieved; null if no image was found.
+     */
+    public static Image getImageFromUrl(final String url) {
+        try {
+            return ImageIO.read(new URL(url));
+        } catch (IOException e) {
+            General.println("Failed to retreive image from: " + url);
+            return null;
+        }
+    }
 }
