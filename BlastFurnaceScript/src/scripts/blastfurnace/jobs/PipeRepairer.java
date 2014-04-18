70983ad27a0e66e17859b6036dd20e3c3feed828
Nolan <9oxsteelflux@gmail.com>
2014-04-18 05:37:25 -0400

    Addition PipeRepairer class
    started work on it, got one pipe id

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
@@ -1,24 +1,22 @@
 package scripts.blastfurnace.jobs;
 
+import org.tribot.api2007.Objects;
 import scripts.blastfurnace.framework.Job;
 
 /**
  * @author Starfox, erickho123
- */ 
+ */
 public class PipeRepairer extends Job {
+
+    private final int[] BROKEN_PIPE_IDS = {9121};
+    
+    @Override
+    public boolean shouldDo() {
+        return Objects.findNearest(40, BROKEN_PIPE_IDS).length > 0;
+    }
+
+    @Override
+    public void doJob() {
 	
-	
-
-	@Override
-	public boolean shouldDo() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void doJob() {
-		// TODO Auto-generated method stub
-		
-	}
-
+    }
 }
