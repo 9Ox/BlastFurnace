a51ff4620ba2c45c70b4fb37872caedad746e5d9
erickho123 <erickho123@gmail.com>
2014-04-17 21:27:48 -0500

    Create PipeRepairer class
    There are a lot of pipes around the area that break a lot. To fix these
    you need lvl 30 crafting, and a hammer. When you fix them, you get 50
    crafting exp.

---
+++ BlastFurnaceScript/src/scripts/jobs/PipeRepairer.java
@@ -0,0 +1,24 @@
+package scripts.jobs;
+
+import scripts.framework.Job;
+
+/**
+ * @author Starfox, erickho123
+ */ 
+public class PipeRepairer extends Job {
+	
+	
+
+	@Override
+	public boolean shouldDo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void doJob() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
