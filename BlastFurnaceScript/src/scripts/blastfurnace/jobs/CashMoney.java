cf254d0fc0a7b682b728965226e05b970287a705
Nolan <9oxsteelflux@gmail.com>
2014-04-18 07:34:06 -0400

    Create CashMoney class

---
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
@@ -0,0 +1,19 @@
+package scripts.blastfurnace.jobs;
+
+import scripts.blastfurnace.framework.Job;
+
+/**
+ * @author Starfox
+ */
+public class CashMoney extends Job {
+
+    @Override
+    public boolean shouldDo() {
+        return true;
+    }
+
+    @Override
+    public void doJob() {
+    }
+
+}
