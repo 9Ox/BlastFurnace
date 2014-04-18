c763b08c3781760f656bfe02ebd4c9be5e7f35c3
Nolan <9oxsteelflux@gmail.com>
2014-04-18 07:33:52 -0400

    IDE format

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/PipeRepairer.java
@@ -7,11 +7,8 @@ import org.tribot.api2007.Objects;
 import org.tribot.api2007.Player;
 import org.tribot.api2007.ext.Filters;
 import org.tribot.api2007.types.RSObject;
-import org.tribot.api2007.types.RSObjectDefinition;
-import org.tribot.api2007.types.RSTile;
 
 import scripts.blastfurnace.framework.Job;
-import scripts.blastfurnace.util.Get;
 import scripts.blastfurnace.util.RSUtil;
 import scripts.blastfurnace.util.Walking;
 
@@ -20,38 +17,38 @@ import scripts.blastfurnace.util.Walking;
  */
 public class PipeRepairer extends Job {
 
-	private final String REPAIR_OPTION = "Repair Pipes";
+    private final String REPAIR_OPTION = "Repair Pipes";
 
-	@Override
-	public boolean shouldDo() {
-		RSObject[] pipes = Objects.findNearest(40, Filters.Objects.actionsEquals(REPAIR_OPTION));
-		return pipes.length > 0;
-	}
+    @Override
+    public boolean shouldDo() {
+        RSObject[] pipes = Objects.findNearest(40, Filters.Objects.actionsEquals(REPAIR_OPTION));
+        return pipes.length > 0;
+    }
 
-	@Override
-	public void doJob() {
-		RSObject pipe = null;
-		RSObject[] pipes = Objects.findNearest(40, Filters.Objects.actionsEquals(REPAIR_OPTION));
-		if(pipes.length > 0) {
-			pipe = pipes[0];
-			if (pipe != null) {
-				if (pipe.isOnScreen() && Player.getAnimation() == -1) {
-					if (RSUtil.clickRSObject(REPAIR_OPTION, pipe)) {
-						Timing.waitCondition(new Condition() {
-							@Override
-							public boolean active() {
-								return Player.getAnimation() != -1;
-							}
-						}, 4000);
-					}
-				} else {
-					if (pipe.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
-						Camera.turnToTile(pipe);
-					} else {
-						Walking.walkTo(pipe.getPosition());
-					}
-				}
-			}
-		}
-	}
+    @Override
+    public void doJob() {
+        RSObject pipe;
+        RSObject[] pipes = Objects.findNearest(40, Filters.Objects.actionsEquals(REPAIR_OPTION));
+        if (pipes.length > 0) {
+            pipe = pipes[0];
+            if (pipe != null) {
+                if (pipe.isOnScreen() && Player.getAnimation() == -1) {
+                    if (RSUtil.clickRSObject(REPAIR_OPTION, pipe)) {
+                        Timing.waitCondition(new Condition() {
+                            @Override
+                            public boolean active() {
+                                return Player.getAnimation() != -1;
+                            }
+                        }, 4000);
+                    }
+                } else {
+                    if (pipe.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+                        Camera.turnToTile(pipe);
+                    } else {
+                        Walking.walkTo(pipe.getPosition());
+                    }
+                }
+            }
+        }
+    }
 }
