feb15228cbbc10d8b7876e0d82966f3da099694f
erickho123 <erickho123@gmail.com>
2014-04-18 19:01:52 -0500

    getPrimaryOre, getSecondaryOre()
    hi

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
@@ -1,5 +1,7 @@
 package scripts.blastfurnace.jobs;
 
+import org.tribot.api2007.Game;
+
 import scripts.blastfurnace.framework.Job;
 
 /**
@@ -7,6 +9,21 @@ import scripts.blastfurnace.framework.Job;
  */
 public class CashMoney extends Job {
 
+	/**
+	 * Gets the amount of your primary ore in the furnace
+	 * @return the main ore amount
+	 */
+	private int getPrimaryOreAmount() {
+		return (Game.getSetting(547) >> 24);
+	}
+	
+	/**
+	 * Gets the amount of your secondary ore in the furnace
+	 * @return the secondary
+	 */
+	private int getSecondaryOreAmount() {
+		return (Game.getSetting(547)) & 0xFE;
+	}
     @Override
     public boolean shouldDo() {
         return true;
