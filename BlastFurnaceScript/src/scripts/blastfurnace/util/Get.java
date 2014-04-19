5887c6d61d15b9f66661487f043428259dc6669b
erickho123 <erickho123@gmail.com>
2014-04-18 20:30:21 -0500

    Additon getBar
    gets the bar from the specified string

--- BlastFurnaceScript/src/scripts/blastfurnace/util/Get.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/Get.java
@@ -9,24 +9,37 @@ import org.tribot.api2007.types.RSObject;
  * @author Starfox
  */
 public class Get {
-    /**
-     * Returns the nearest RSObject with the specified id(s) and within the specified distance.
-     * @param dist Distance to stay within.
-     * @param ids The id(s) to check for.
-     * @return The nearest RSObject found; null if none were found.
-     */
-    public static RSObject getObject(final int dist, final int... ids) {
-        RSObject[] objs = Objects.findNearest(dist, ids);
-        return objs.length > 0 ? objs[0] : null;
-    }
-    
-    /**
-     * Returns the nearest RSNPC with the specified id(s).
-     * @param ids The id(s) to check for.
-     * @return The nearest RSNPC found; null if none were found.
-     */
-    public static RSNPC getNpc(final int... ids) {
-        RSNPC[] npcs = NPCs.find(ids);
-        return npcs.length > 0 ? npcs[0] : null;
-    }
+	/**
+	 * Returns the nearest RSObject with the specified id(s) and within the specified distance.
+	 * @param dist Distance to stay within.
+	 * @param ids The id(s) to check for.
+	 * @return The nearest RSObject found; null if none were found.
+	 */
+	public static RSObject getObject(final int dist, final int... ids) {
+		RSObject[] objs = Objects.findNearest(dist, ids);
+		return objs.length > 0 ? objs[0] : null;
+	}
+
+	/** 
+	 * Returns the Bar from the specified String.
+	 * @param barName The name of the bar
+	 * Returns the bar from found the string; null if none is found
+	 */
+	public static Bar getBar(String barName) {
+		for(Bar b : Bar.values()) {
+			if(b.name().equalsIgnoreCase(barName)) {
+				return b;
+			}
+		}
+		return null;
+	}
+	/**
+	 * Returns the nearest RSNPC with the specified id(s).
+	 * @param ids The id(s) to check for.
+	 * @return The nearest RSNPC found; null if none were found.
+	 */
+	public static RSNPC getNpc(final int... ids) {
+		RSNPC[] npcs = NPCs.find(ids);
+		return npcs.length > 0 ? npcs[0] : null;
+	}
 }
