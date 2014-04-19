d6e0c03be2d896930fbe371283185d38bf4d8f04
erickho123 <erickho123@gmail.com>
2014-04-18 20:31:06 -0500

    Base skeleton to CashMoney

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
@@ -3,12 +3,45 @@ package scripts.blastfurnace.jobs;
 import org.tribot.api2007.Game;
 
 import scripts.blastfurnace.framework.Job;
+import scripts.blastfurnace.util.Bar;
 
 /**
  * @author Starfox
  */
 public class CashMoney extends Job {
 
+	public static Bar barType;
+	private final int MAX_PRIMARY_AMOUNT = 28;
+	private final int MAX_SECONDARY_AMOUNT = 254;
+
+
+
+	@Override
+	public boolean shouldDo() {
+		return getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT || getSecondaryOreAmount() != MAX_PRIMARY_AMOUNT || barType.getAmountStored() > 0 || needsToCool();
+	}
+
+	@Override
+	public void doJob() {
+		if(needsToCool()) {
+			new Cooler().doJob(); // dat laze
+		} else if(barType.getAmountStored() > 0) {
+			collectBars();
+		} else if(getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT) {
+
+		} else if(getSecondaryOreAmount() != MAX_SECONDARY_AMOUNT)  {
+
+		}
+	}
+
+
+	/**
+	 * Checks if it needs to cool the dispesner
+	 * @return true if it needs to cool the dispenser; returns false if it does not.
+	 */
+	private boolean needsToCool() {
+		return false;
+	}
 	/**
 	 * Gets the amount of your primary ore in the furnace
 	 * @return the main ore amount
@@ -16,7 +49,7 @@ public class CashMoney extends Job {
 	private int getPrimaryOreAmount() {
 		return (Game.getSetting(547) >> 24);
 	}
-	
+
 	/**
 	 * Gets the amount of your secondary ore in the furnace
 	 * @return the secondary
@@ -24,13 +57,11 @@ public class CashMoney extends Job {
 	private int getSecondaryOreAmount() {
 		return (Game.getSetting(547)) & 0xFE;
 	}
-    @Override
-    public boolean shouldDo() {
-        return true;
-    }
 
-    @Override
-    public void doJob() {
-    }
+	/**
+	 * Collects bars from the dispense
+	 */
+	private void collectBars() {
 
+	}
 }
