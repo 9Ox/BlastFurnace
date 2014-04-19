5dba4836db896e7c1d392e6850061dcc2d14b81d
erickho123 <erickho123@gmail.com>
2014-04-19 17:18:29 -0500

    Modification to world hop to starting world

--- BlastFurnaceScript/src/scripts/blastfurnace/framework/JobLoop.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/framework/JobLoop.java
@@ -1,56 +1,70 @@
 package scripts.blastfurnace.framework;
 
 import org.tribot.api.General;
+import org.tribot.api2007.Game;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.StartWithExtensionsType;
+
+import scripts.blastfurnace.BlastFurnace;
+import scripts.blastfurnace.util.WorldHop;
+import sun.security.acl.WorldGroupImpl;
 
 /**
  * @author Starfox
  */
 public class JobLoop {
-    private static boolean ready = false;
-     
-    /**
-     * Should be called once all the desired Jobs and terminate conditions have been added to the JobManager.
-     * @param b true to set the script to be ready; false otherwise.
-     */
-    public static void setReady(boolean b) {
-        ready = b;
-    }
-    
-    /**
-     * Automatically called in the waitUntilReady() method.
-     * @return true if the script is ready to start; false otherwise.
-     */
-    private static boolean isReady() {
-        return ready;
-    }
-    
-    /**
-     * Automatically called in the loop() method. Will wait indefinitely until the script is set to be ready.
-     * This method will also load any jobs that were added in the JobLoader class.
-     */
-    private static void waitUntilReady() {
-        while (!isReady()) {
-            General.sleep(10);
-        }
-        JobLoader.loadJobs();
-    }
-    
-    /**
-     * Waits for the script to be ready, then loops through the JobManager's jobs.
-     * In each iteration of the loop, this method will check to see if any terminate conditions are met; if any are met, the method will return.
-     * This method is automatically called in the start() method.
-     */
-    private static void loop() {
-        waitUntilReady();
-        while (!JobManager.shouldTerminate()) {
-            JobManager.runJobs();
-        }
-    }
-    
-    /**
-     * Starts the JobLoop.
-     */
-    public static void start() {
-        loop();
-    }
+	private static boolean ready = false;
+
+	/**
+	 * Should be called once all the desired Jobs and terminate conditions have been added to the JobManager.
+	 * @param b true to set the script to be ready; false otherwise.
+	 */
+	public static void setReady(boolean b) {
+		ready = b;
+	}
+
+	/**
+	 * Automatically called in the waitUntilReady() method.
+	 * @return true if the script is ready to start; false otherwise.
+	 */
+	private static boolean isReady() {
+		return ready;
+	}
+
+	/**
+	 * Automatically called in the loop() method. Will wait indefinitely until the script is set to be ready.
+	 * This method will also load any jobs that were added in the JobLoader class.
+	 */
+	private static void waitUntilReady() {
+		while (!isReady()) {
+			General.sleep(10);
+		}
+		JobLoader.loadJobs();
+	}
+
+	/**
+	 * Waits for the script to be ready, then loops through the JobManager's jobs.
+	 * In each iteration of the loop, this method will check to see if any terminate conditions are met; if any are met, the method will return.
+	 * This method is automatically called in the start() method.
+	 */
+	private static void loop() {
+		waitUntilReady();
+		while (!JobManager.shouldTerminate()) {
+			if(Game.getCurrentWorld() != BlastFurnace.START_WORLD) {
+				BlastFurnace.script.setLoginBotState(false);
+				if(WorldHop.switchWorld(BlastFurnace.START_WORLD)) {
+					BlastFurnace.script.setLoginBotState(false);
+				}
+			} else {
+				JobManager.runJobs();
+			}
+		}
+	}
+
+	/**
+	 * Starts the JobLoop.
+	 */
+	public static void start() {
+		loop();
+	}
 }
