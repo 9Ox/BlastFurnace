fa7c35485a74b3f90f2b38eabccc0a0baf2a8e53
Nolan <9oxsteelflux@gmail.com>
2014-04-19 15:21:44 -0400

    Removed anonymous Cooler
    Added Cooler as instance variable, that swag.

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
@@ -10,58 +10,55 @@ import scripts.blastfurnace.util.Bar;
  */
 public class CashMoney extends Job {
 
-	public static Bar barType;
-	private final int MAX_PRIMARY_AMOUNT = 28;
-	private final int MAX_SECONDARY_AMOUNT = 254;
+    public static Bar barType;
+    private final int MAX_PRIMARY_AMOUNT = 28;
+    private final int MAX_SECONDARY_AMOUNT = 254;
+    private final Cooler cooler;
+    
+    public CashMoney() {
+        cooler = new Cooler();
+    }
 
+    @Override
+    public boolean shouldDo() {
+        return getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT || getSecondaryOreAmount() != MAX_PRIMARY_AMOUNT || barType.getAmountStored() > 0 || cooler.shouldDo();
+    }
 
+    @Override
+    public void doJob() {
+        if (cooler.shouldDo()) {
+            cooler.doJob(); // dat laze
+        } else if (barType.getAmountStored() > 0) {
+            collectBars();
+        } else if (getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT) {
 
-	@Override
-	public boolean shouldDo() {
-		return getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT || getSecondaryOreAmount() != MAX_PRIMARY_AMOUNT || barType.getAmountStored() > 0 || needsToCool();
-	}
+        } else if (getSecondaryOreAmount() != MAX_SECONDARY_AMOUNT) {
 
-	@Override
-	public void doJob() {
-		if(needsToCool()) {
-			new Cooler().doJob(); // dat laze
-		} else if(barType.getAmountStored() > 0) {
-			collectBars();
-		} else if(getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT) {
+        }
+    }
 
-		} else if(getSecondaryOreAmount() != MAX_SECONDARY_AMOUNT)  {
+    /**
+     * Gets the amount of your primary ore in the furnace
+     *
+     * @return the main ore amount
+     */
+    private int getPrimaryOreAmount() {
+        return (Game.getSetting(547) >> 24);
+    }
 
-		}
-	}
+    /**
+     * Gets the amount of your secondary ore in the furnace
+     *
+     * @return the secondary
+     */
+    private int getSecondaryOreAmount() {
+        return (Game.getSetting(547)) & 0xFE;
+    }
 
+    /**
+     * Collects bars from the dispense
+     */
+    private void collectBars() {
 
-	/**
-	 * Checks if it needs to cool the dispesner
-	 * @return true if it needs to cool the dispenser; returns false if it does not.
-	 */
-	private boolean needsToCool() {
-		return false;
-	}
-	/**
-	 * Gets the amount of your primary ore in the furnace
-	 * @return the main ore amount
-	 */
-	private int getPrimaryOreAmount() {
-		return (Game.getSetting(547) >> 24);
-	}
-
-	/**
-	 * Gets the amount of your secondary ore in the furnace
-	 * @return the secondary
-	 */
-	private int getSecondaryOreAmount() {
-		return (Game.getSetting(547)) & 0xFE;
-	}
-
-	/**
-	 * Collects bars from the dispense
-	 */
-	private void collectBars() {
-
-	}
+    }
 }
