afe3b17a652b967ef74b7c0a084e5845ec22369f
erickho123 <erickho123@gmail.com>
2014-04-20 03:18:28 -0500

    Added Gauge clicking using Get#getRSObject(RSTile tile)

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/ShoutCaller.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/ShoutCaller.java
@@ -3,16 +3,23 @@ package scripts.blastfurnace.jobs;
 import org.tribot.api.Timing;
 import org.tribot.api.input.Keyboard;
 import org.tribot.api.types.generic.Condition;
+import org.tribot.api2007.Camera;
 import org.tribot.api2007.Interfaces;
+import org.tribot.api2007.Player;
 import org.tribot.api2007.types.RSInterfaceChild;
+import org.tribot.api2007.types.RSObject;
+import org.tribot.api2007.types.RSTile;
 
 import scripts.blastfurnace.framework.Job;
+import scripts.blastfurnace.util.Get;
+import scripts.blastfurnace.util.RSUtil;
+import scripts.blastfurnace.util.Walking;
 
 public class ShoutCaller extends Job {
 
     private final int PARENT_ID = 30;
     private final int CHILD_ID = 3;
-
+    private final RSTile GAUGE_TILE = new RSTile(1945,4961,0);
     @Override
     public boolean shouldDo() {
         RSInterfaceChild gauge = Interfaces.get(PARENT_ID, CHILD_ID);
@@ -48,7 +55,26 @@ public class ShoutCaller extends Job {
     /**
      * Clicks the gauge, will walk to the object if it's not on screen
      */
-    public static void clickGauge() {
+    private void clickGauge() {
+    	RSObject gauge = Get.getObject(GAUGE_TILE);
+    	if(gauge != null) {
+    		if(gauge.isOnScreen()) {
+    			if(RSUtil.clickRSObject("Read", gauge))
+    				   Timing.waitCondition(new Condition() {
+    	                    @Override
+    	                    public boolean active() {
+    	                        return Interfaces.get(PARENT_ID, CHILD_ID) != null;
+    	                    }
+    	                }, 2000);
+    		} else {
+    			 if (gauge.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+                     Camera.turnToTile(gauge);
+                 } else {
+                     Walking.walkTo(gauge.getPosition());
+                 }
+    		}
+    	}
+    	
 
     }
 
