fe0e7e0beb98274d60540a5934751c62768dd8a2
Nolan <9oxsteelflux@gmail.com>
2014-04-20 02:56:54 -0400

    Added ore ids to enum

--- BlastFurnaceScript/src/scripts/blastfurnace/util/Bar.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/Bar.java
@@ -9,19 +9,27 @@ import org.tribot.api2007.types.RSInterfaceChild;
  * @author Nolan
  */
 public enum Bar {
-    BRONZE(108, 0), IRON(109, 8), STEEL(110, 16), MITHRIL(111, 24), ADAMANT(112, 32), RUNE(113, 40), SILVER(114, 48), GOLD(115, 56);
+    BRONZE(108, 0, new int[]{436, 438}), IRON(109, 8, new int[]{440}), STEEL(110, 16, new int[]{440, 453}), MITHRIL(111, 24, new int[]{447, 453}), 
+    ADAMANT(112, 32, new int[]{449, 453}), RUNE(113, 40, new int[]{451, 453}), SILVER(114, 48, new int[]{442}), GOLD(115, 56, new int[]{444});
     
     private final int MASTER_INDEX = 28;
     private final int SETTING_INDEX = 545;
     private final int childIndex;
     private final int bits;
-    Bar(final int childIndex, final int bits) {
+    private final int[] ores;
+    
+    Bar(final int childIndex, final int bits, int[] ores) {
         this.childIndex = childIndex;
         this.bits = bits;
+        this.ores = ores;
     }
     
     private int getChildIndex() {
         return this.childIndex;
+    }
+    
+    private int[] getOres() {
+        return this.ores;
     }
     
     public RSInterfaceChild getInterface() {
@@ -35,5 +43,9 @@ public enum Bar {
     
     public int getAmountStored() {
     	return Game.getSetting(SETTING_INDEX) >> bits;
+    }
+    
+    public boolean requiresSeconary() {
+        return getOres().length > 1;
     }
 }
