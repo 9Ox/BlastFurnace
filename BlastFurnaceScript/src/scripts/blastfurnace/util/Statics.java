63455a708a81c0c306ad84ba5cd5d06efd676a5c
Nolan <9oxsteelflux@gmail.com>
2014-04-20 21:04:51 -0400

    debugs

--- BlastFurnaceScript/src/scripts/blastfurnace/util/Statics.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/Statics.java
@@ -7,4 +7,7 @@ public class Statics {
     public static String shoutCallerName = "";
     public static boolean startPumping = false;
     public static int startWorld;
+    public static String jobName = "";
+    public static boolean doJob = false;
+    public static String bar = "";
 }
