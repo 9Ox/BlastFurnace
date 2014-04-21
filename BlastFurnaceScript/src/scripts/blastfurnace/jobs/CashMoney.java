282095b27bf6ec09c2fccdc2d43058bd448f7ef4
erickho123 <erickho123@gmail.com>
2014-04-21 01:31:18 -0500

    More adjustments
    hi

--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/CashMoney.java
@@ -27,219 +27,228 @@ import scripts.blastfurnace.util.Walking;
  */
 public class CashMoney extends Job {
 
-    private final String barName;
-    private final int MAX_PRIMARY_AMOUNT = 27;
+	private final String barName;
+	private final int MAX_PRIMARY_AMOUNT = 27;
 
-    private final int MAX_SECONDARY_AMOUNT = 200;
-    private final RSTile[] PATH_TO_FURNACE = { new RSTile(1946, 4959, 0), new RSTile(1941, 4960, 0), new RSTile(1938, 4962, 0), new RSTile(1937, 4966, 0), new RSTile(1939, 4967, 0) };
-    private final RSTile[] PATH_TO_BANK = org.tribot.api2007.Walking.invertPath(PATH_TO_FURNACE);
+	private final int MAX_SECONDARY_AMOUNT = 200;
+	private final RSTile[] PATH_TO_FURNACE = { new RSTile(1946, 4959, 0), new RSTile(1941, 4960, 0), new RSTile(1938, 4962, 0), new RSTile(1937, 4966, 0), new RSTile(1939, 4967, 0) };
+	private final RSTile[] PATH_TO_BANK = org.tribot.api2007.Walking.invertPath(PATH_TO_FURNACE);
 
-    private final RSTile BAR_DISPENSER_TILE = new RSTile(1940, 4963, 0);
-    private final RSTile BANK_CHEST_TILE = new RSTile(1948, 4956, 0);
-    private final RSTile CONVEYOR_BELT_TILE = new RSTile(1943, 4967, 0);
-    private final Cooler cooler;
-    private final Bar barType;
+	private final RSTile BAR_DISPENSER_TILE = new RSTile(1940, 4963, 0);
+	private final RSTile BANK_CHEST_TILE = new RSTile(1948, 4956, 0);
+	private final RSTile CONVEYOR_BELT_TILE = new RSTile(1943, 4967, 0);
+	private final Cooler cooler;
+	private final Bar barType;
 
-    public CashMoney(Bar barType) {
-        cooler = new Cooler();
-        this.barType = barType;
-        String string = barType.name().toLowerCase().concat(" bar");
-        barName = Character.toString(string.charAt(0)).toUpperCase() + string.substring(1);
-    }
+	public CashMoney(Bar barType) {
+		cooler = new Cooler();
+		this.barType = barType;
+		String string = barType.name().toLowerCase().concat(" bar");
+		barName = Character.toString(string.charAt(0)).toUpperCase() + string.substring(1);
+	}
 
-    @Override
-    public boolean shouldDo() {
-        return getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT || getSecondaryOreAmount() < MAX_SECONDARY_AMOUNT || barType.getAmountStored() > 0 || cooler.shouldDo()
-                || Inventory.getCount(barName) > 0;
-    }
+	@Override
+	public boolean shouldDo() {
 
-    @Override
-    public void doJob() {
-        if (Inventory.getCount(barName) > 0) {
-            bankBars();
-        } else if (cooler.shouldDo()) {
-            cooler.doJob(); // dat laze
-        } else if (barType.getAmountStored() > 0) {
-            collectBars();
-        } else if (barType.requiresSeconary() && getSecondaryOreAmount() < MAX_SECONDARY_AMOUNT) {
-            if (Inventory.getCount(barType.getOres()[1]) == 0) {
-                getOres(barType.getOres()[1]);
-            } else {
-                putOresIn(barType.getOres()[1]);
-            }
-        } else if (getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT) {
-        	General.println(getPrimaryOreAmount());
-            if (Inventory.getCount(barType.getOres()[0]) == 0) {
-                getOres(barType.getOres()[0]);
-            } else {
-                putOresIn(barType.getOres()[0]);
-            }
-        }
+		return getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT || getSecondaryOreAmount() < MAX_SECONDARY_AMOUNT || barType.getAmountStored() > 0 || cooler.shouldDo()
+				|| Inventory.getCount(barName) > 0;
+	}
 
-    }
+	@Override
+	public void doJob() {
 
-    /**
-     * Gets the amount of your primary ore in the furnace
-     *
-     * @return the main ore amount
-     */
-    private int getPrimaryOreAmount() {
-        return (Game.getSetting(547) >> 16);
-    }
+		if (Inventory.getCount(barName) > 0) {
+			bankBars();
+		} else if (isBarsCooled() && barType.getAmountStored() > 0) {
+			collectBars();
+		} else if (cooler.shouldDo()) {
+			cooler.doJob(); // dat laze
+		} else if (barType.requiresSeconary() && getSecondaryOreAmount() < MAX_SECONDARY_AMOUNT) {
+			if (Inventory.getCount(barType.getOres()[1]) == 0) {
+				getOres(barType.getOres()[1]);
+			} else {
+				putOresIn(barType.getOres()[1]);
+			}
+		} else if (getPrimaryOreAmount() != MAX_PRIMARY_AMOUNT) {
 
-    /**
-     * Gets the amount of your secondary ore in the furnace
-     *
-     * @return the secondary ore amount
-     */
-    private int getSecondaryOreAmount() {
-        return (Game.getSetting(547)) & 0xFE;
-    }
+			if (Inventory.getCount(barType.getOres()[0]) == 0) {
+				getOres(barType.getOres()[0]);
+			} else {
+				putOresIn(barType.getOres()[0]);
+			}
+		}
+	}
 
-    /**
-     * Collects bars from the dispense
-     */
-    private void collectBars() {
-        RSObject dispenser = Get.getObject(BAR_DISPENSER_TILE);
-        if (Inventory.isFull()) {
-            bankBars();
-        } else if (barType.getInterface() == null) {
-            if (dispenser != null) {
-                if (dispenser.isOnScreen()) {
-                    if (RSUtil.clickRSObject("Take", dispenser)) {
-                        Timing.waitCondition(new Condition() {
-                            @Override
-                            public boolean active() {
-                                return barType.getInterface() != null;
-                            }
-                        }, 3000);
-                    }
-                } else {
-                    if (dispenser.getPosition().distanceTo(Player.getPosition()) <= 4) {
-                        Camera.turnToTile(dispenser);
-                    } else {
-                        Walking.blindWalkTo(dispenser.getPosition());
-                    }
-                }
-            }
-        } else {
-            if (barType.clickInterface("Take")) {
-                Timing.waitCondition(new Condition() {
-                    @Override
-                    public boolean active() {
-                        return Inventory.getCount(barName) > 0;
-                    }
-                }, 2000);
-            }
-        }
-    }
+	/**
+	 * Checks whether the bars is cooled
+	 * @return Returns true if bars are cooled, else false
+	 */
+	private boolean isBarsCooled() {
+		return Game.getSetting(543) == 768;
+	}
 
-    /**
-     * Deposits bars.
-     */
-    private void bankBars() {
-    	if(barType.getInterface() != null) {
-    		Interfaces.closeAll();
-    	} else if (!Banking.isBankScreenOpen()) {
-            openBankChest();
-        } else {
-            if (Banking.depositAllExcept(Cooler.EMPTY_BUCKET_ID, Cooler.FULL_BUCKET_ID) > 0) {
-                Timing.waitCondition(new Condition() {
-                    @Override
-                    public boolean active() {
-                        return Inventory.getCount(barName) <= 0;
-                    }
-                }, 2000);
-            }
-        }
-    }
+	/**
+	 * Gets the amount of your primary ore in the furnace
+	 *
+	 * @return the main ore amount
+	 */
+	private int getPrimaryOreAmount() {
+		return (Game.getSetting(547) >> 16);
+	}
 
-    /**
-     * Puts ores in the furnace. Withdraws ores if none are in inventory.
-     */
-    private void putOresIn(final int id) {
-            String[] npcOptions = NPCChat.getOptions();
-            if (npcOptions != null && npcOptions.length > 0 && Arrays.asList(npcOptions).contains("Yes")) {
-                NPCChat.selectOption("Yes", true);
-            } else {
-                RSObject belt = Get.getObject(CONVEYOR_BELT_TILE);
-                if (belt != null) {
-                    if (belt.isOnScreen()) {
-                        if (Banking.isBankScreenOpen()) {
-                            Banking.close();
-                        } else if (RSUtil.clickRSObject("Put-ore-on", belt)) {
-                            Timing.waitCondition(new Condition() {
+	/**
+	 * Gets the amount of your secondary ore in the furnace
+	 *
+	 * @return the secondary ore amount
+	 */
+	private int getSecondaryOreAmount() {
+		return (Game.getSetting(547)) & 0xFE;
+	}
 
-                                @Override
-                                public boolean active() {
-                                    String[] npcOptions = NPCChat.getOptions();
-                                    return npcOptions != null && npcOptions.length > 0 && Arrays.asList(npcOptions).contains("Yes");
-                                }
-                            }, 9000);
-                        }
-                    } else {
-                        if (belt.getPosition().distanceTo(Player.getPosition()) <= 4) {
-                            Camera.turnToTile(belt);
-                        } else {
-                            Walking.walkPath(false, PATH_TO_FURNACE);
-                            Camera.setCameraAngle(100);
-                        }
-                    }
-                }
-            }
-        
-    }
+	/**
+	 * Collects bars from the dispense
+	 */
+	private void collectBars() {
+		RSObject dispenser = Get.getObject(BAR_DISPENSER_TILE);
+		if (Inventory.isFull()) {
+			bankBars();
+		} else if (barType.getInterface() == null) {
+			if (dispenser != null) {
+				if (dispenser.isOnScreen()) {
+					if (RSUtil.clickRSObject("Take", dispenser)) {
+						Timing.waitCondition(new Condition() {
+							@Override
+							public boolean active() {
+								return barType.getInterface() != null;
+							}
+						}, 3000);
+					}
+				} else {
+					if (dispenser.getPosition().distanceTo(Player.getPosition()) <= 4) {
+						Camera.turnToTile(dispenser);
+					} else {
+						Walking.blindWalkTo(dispenser.getPosition());
+					}
+				}
+			}
+		} else {
+			if (barType.clickInterface("Take")) {
+				Timing.waitCondition(new Condition() {
+					@Override
+					public boolean active() {
+						return Inventory.getCount(barName) > 0;
+					}
+				}, 2000);
+			}
+		}
+	}
 
-    /**
-     * Withdraws the specified ore id from the bank
-     *
-     * @param id The ore id to withdraw
-     */
-    private void getOres(final int id) {
-        if (Inventory.getCount(id) <= 0) {
-            if (!Banking.isBankScreenOpen()) {
-                openBankChest();
-            } else {
-                Banking.depositAllExcept(Cooler.FULL_BUCKET_ID, Cooler.EMPTY_BUCKET_ID);
-                RSItem[] ore = Banking.find(id);
-                if (ore.length > 0) {
-                    if (ore[0] != null) {
-                        if (Banking.withdrawItem(ore[0], MAX_PRIMARY_AMOUNT - getPrimaryOreAmount())) {
-                            Timing.waitCondition(new Condition() {
-                                @Override
-                                public boolean active() {
-                                    return Inventory.getCount(id) > 0;
-                                }
-                            }, 3000);
-                        }
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * Deposits bars.
+	 */
+	private void bankBars() {
+		if (!Banking.isBankScreenOpen()) {
+			openBankChest();
+		} else {
+			if (Banking.depositAllExcept(Cooler.EMPTY_BUCKET_ID, Cooler.FULL_BUCKET_ID) > 0) {
+				Timing.waitCondition(new Condition() {
+					@Override
+					public boolean active() {
+						return Inventory.getCount(barName) <= 0;
+					}
+				}, 2000);
+			}
+		}
+	}
 
-    /**
-     * Opens bank chest
-     */
-    private void openBankChest() {
-        RSObject chest = Get.getObject(BANK_CHEST_TILE);
-        if (chest != null) {
-            if (chest.isOnScreen()) {
-                if (RSUtil.clickRSObject("Use Bank chest", chest)) {
-                    Timing.waitCondition(new Condition() {
-                        @Override
-                        public boolean active() {
-                            return Banking.isBankScreenOpen();
-                        }
-                    }, 3000);
-                }
-            } else {
-                if (chest.getPosition().distanceTo(Player.getPosition()) <= 4) {
-                    Camera.turnToTile(chest);
-                } else {
-                    Walking.walkPath(false, PATH_TO_BANK);
-                }
-            }
-        }
-    }
+	/**
+	 * Puts ores in the furnace. Withdraws ores if none are in inventory.
+	 */
+	private void putOresIn(final int id) {
+		String[] npcOptions = NPCChat.getOptions();
+		if (npcOptions != null && npcOptions.length > 0 && Arrays.asList(npcOptions).contains("Yes")) {
+			NPCChat.selectOption("Yes", true);
+		} else {
+			RSObject belt = Get.getObject(CONVEYOR_BELT_TILE);
+			if (belt != null) {
+				if (belt.isOnScreen()) {
+					if (Banking.isBankScreenOpen()) {
+						Banking.close();
+					} else if (RSUtil.clickRSObject("Put-ore-on", belt)) {
+						Timing.waitCondition(new Condition() {
+
+							@Override
+							public boolean active() {
+								String[] npcOptions = NPCChat.getOptions();
+								return npcOptions != null && npcOptions.length > 0 && Arrays.asList(npcOptions).contains("Yes");
+							}
+						}, 9000);
+					}
+				} else {
+					if (belt.getPosition().distanceTo(Player.getPosition()) <= 4) {
+						Camera.turnToTile(belt);
+					} else {
+						Walking.walkPath(false, PATH_TO_FURNACE);
+						Camera.setCameraAngle(100);
+					}
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Withdraws the specified ore id from the bank
+	 *
+	 * @param id The ore id to withdraw
+	 */
+	private void getOres(final int id) {
+		if (Inventory.getCount(id) <= 0) {
+			if (!Banking.isBankScreenOpen()) {
+				openBankChest();
+			} else {
+				Banking.depositAllExcept(Cooler.FULL_BUCKET_ID, Cooler.EMPTY_BUCKET_ID);
+				RSItem[] ore = Banking.find(id);
+				if (ore.length > 0) {
+					if (ore[0] != null) {
+						if (Banking.withdrawItem(ore[0], MAX_PRIMARY_AMOUNT - getPrimaryOreAmount())) {
+							Timing.waitCondition(new Condition() {
+								@Override
+								public boolean active() {
+									return Inventory.getCount(id) > 0;
+								}
+							}, 3000);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Opens bank chest
+	 */
+	private void openBankChest() {
+		RSObject chest = Get.getObject(BANK_CHEST_TILE);
+		if (chest != null) {
+			if (chest.isOnScreen()) {
+				if(barType.getInterface() != null) {
+					Interfaces.closeAll();
+				} else if (RSUtil.clickRSObject("Use Bank chest", chest)) {
+					Timing.waitCondition(new Condition() {
+						@Override
+						public boolean active() {
+							return Banking.isBankScreenOpen();
+						}
+					}, 3000);
+				}
+			} else {
+				if (chest.getPosition().distanceTo(Player.getPosition()) <= 4) {
+					Camera.turnToTile(chest);
+				} else {
+					Walking.walkPath(false, PATH_TO_BANK);
+				}
+			}
+		}
+	}
 }
--- BlastFurnaceScript/src/scripts/blastfurnace/jobs/Cooler.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/jobs/Cooler.java
@@ -1,7 +1,6 @@
 package scripts.blastfurnace.jobs;
 
 import org.tribot.api.Clicking;
-import org.tribot.api.General;
 import org.tribot.api.Timing;
 import org.tribot.api.types.generic.Condition;
 import org.tribot.api2007.Camera;
@@ -23,120 +22,128 @@ import scripts.blastfurnace.util.Walking;
  */
 public class Cooler extends Job {
 
-    public static final int EMPTY_BUCKET_ID = 1925;
-    public static final int FULL_BUCKET_ID = 1929;
-    private final RSTile SINK_TILE = new RSTile(1942, 4956, 0);
-    private final RSTile BAR_DISPENSER_TILE = new RSTile(1940, 4963, 0);
-    private final int DISPENSER_SETTING_INDEX = 543;
+	public static final int EMPTY_BUCKET_ID = 1925;
+	public static final int FULL_BUCKET_ID = 1929;
+	private final RSTile SINK_TILE = new RSTile(1942, 4956, 0);
+	private final RSTile BAR_DISPENSER_TILE = new RSTile(1940, 4963, 0);
+	private final int DISPENSER_SETTING_INDEX = 543;
 
-    @Override
-    public boolean shouldDo() {
-        return needsToCoolBar() || ((Inventory.getCount(EMPTY_BUCKET_ID) <= 0 && Inventory.getCount(FULL_BUCKET_ID) <= 0)
-                || Inventory.getCount(EMPTY_BUCKET_ID) > 0);
-    }
+	@Override
+	public boolean shouldDo() {
+		return needsToCoolBar() || ((Inventory.getCount(EMPTY_BUCKET_ID) <= 0 && Inventory.getCount(FULL_BUCKET_ID) <= 0)
+				|| Inventory.getCount(EMPTY_BUCKET_ID) > 0);
+	}
 
-    @Override
-    public void doJob() {
-        if (Inventory.getCount(EMPTY_BUCKET_ID) <= 0 && Inventory.getCount(FULL_BUCKET_ID) <= 0) {
-            getBucket();
-        } else if (Inventory.getCount(EMPTY_BUCKET_ID) > 0) {
-            fillBucket();
-        } else {
-            coolBars();
-        }
-    }
+	@Override
+	public void doJob() {
+		if (Inventory.getCount(EMPTY_BUCKET_ID) <= 0 && Inventory.getCount(FULL_BUCKET_ID) <= 0) {
+			getBucket();
+		} else if (Inventory.getCount(EMPTY_BUCKET_ID) > 0) {
+			fillBucket();
+		} else {
+			coolBars();
+		}
+	}
 
-  
-    /**
-     * Checks if the dispesner needs to be cooled or not
-     *
-     * @return Returns true if it needs to be cooled; false if not.
-     */
-    private boolean needsToCoolBar() {
-        return (Game.getSetting(DISPENSER_SETTING_INDEX)) == 512 ||  (Game.getSetting(DISPENSER_SETTING_INDEX)) == 256;
-    }
 
-    /**
-     * Picks up the bucket in the blast furnace.
-     */
-    private void getBucket() {
-        RSGroundItem[] buckets = GroundItems.find(EMPTY_BUCKET_ID);
-        RSGroundItem bucket;
-        if (buckets.length > 0) {
-            bucket = buckets[0];
-            if (bucket != null) {
-                if (bucket.isOnScreen()) {
-                    if (RSUtil.clickRSGroundItem("Take Bucket", bucket)) {
-                        Timing.waitCondition(new Condition() {
-                            @Override
-                            public boolean active() {
-                                return Inventory.getCount(EMPTY_BUCKET_ID) > 0;
-                            }
-                        }, 4000);
-                    }
-                } else {
-                    if (bucket.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
-                        Camera.turnToTile(bucket);
-                    } else {
-                        Walking.blindWalkTo(bucket.getPosition());
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * Checks if the dispesner needs to be cooled or not
+	 *
+	 * @return Returns true if it needs to be cooled; false if not.
+	 */
+	private boolean needsToCoolBar() {
+		return (Game.getSetting(DISPENSER_SETTING_INDEX)) == 512 ||  (Game.getSetting(DISPENSER_SETTING_INDEX)) == 256;
+	}
 
-    /**
-     * Fills an empty bucket in the blast furnace sink.
-     */
-    private void fillBucket() {
-        RSObject sink = Get.getObject(SINK_TILE);
-        if (sink != null) {
-            if (sink.isOnScreen()) {
-                if (Clicking.click("Use", Inventory.find(EMPTY_BUCKET_ID))) {
-                    if (Game.getUptext().contains("Use Bucket ->")) {
-                        if (RSUtil.clickRSObject("Use Bucket -> Sink", sink)) {
-                            Timing.waitCondition(new Condition() {
-                                @Override
-                                public boolean active() {
-                                    return Inventory.getCount(FULL_BUCKET_ID) > 0;
-                                }
-                            }, 4000);
-                        }
-                    }
-                }
-            } else {
-                if (sink.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
-                    Camera.turnToTile(sink);
-                } else {
-                    Walking.blindWalkTo(sink.getPosition());
-                }
-            }
-        }
-    }
+	/**
+	 * Picks up the bucket in the blast furnace.
+	 */
+	private void getBucket() {
+		RSGroundItem[] buckets = GroundItems.find(EMPTY_BUCKET_ID);
+		RSGroundItem bucket;
+		if (buckets.length > 0) {
+			bucket = buckets[0];
+			if (bucket != null) {
+				if (bucket.isOnScreen()) {
+					if (RSUtil.clickRSGroundItem("Take Bucket", bucket)) {
+						Timing.waitCondition(new Condition() {
+							@Override
+							public boolean active() {
+								return Inventory.getCount(EMPTY_BUCKET_ID) > 0;
+							}
+						}, 4000);
+					}
+				} else {
+					if (bucket.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+						Camera.turnToTile(bucket);
+					} else {
+						Walking.blindWalkTo(bucket.getPosition());
+					}
+				}
+			}
+		}
+	}
 
-    private void coolBars() {
-        RSObject dispenser = Get.getObject(BAR_DISPENSER_TILE);
-        if (dispenser != null) {
-            if (dispenser.isOnScreen()) {
-                if (Clicking.click("Use", Inventory.find(FULL_BUCKET_ID))) {
-                    if (Game.getUptext().contains("Use Bucket of water ->")) {
-                        if (dispenser.click("Use Bucket of water -> Bar dispenser")) {
-                            Timing.waitCondition(new Condition() {
-                                @Override
-                                public boolean active() {
-                                    return false; //check if bars cooled
-                                }
-                            }, 3000);
-                        }
-                    }
-                }
-            } else {
-                if (dispenser.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
-                    Camera.turnToTile(dispenser);
-                } else {
-                    Walking.blindWalkTo(dispenser.getPosition());
-                }
-            }
-        }
-    }
+	/**
+	 * Fills an empty bucket in the blast furnace sink.
+	 */
+	private void fillBucket() {
+		RSObject sink = Get.getObject(SINK_TILE);
+		if (sink != null) {
+			if (sink.isOnScreen()) {
+				String uptext = Game.getUptext();
+				if(uptext != null) {
+					if(uptext.contains("->")) {
+						if (RSUtil.clickRSObject("Use Bucket -> Sink", sink))
+							Timing.waitCondition(new Condition() {
+								@Override
+								public boolean active() {
+									return Inventory.getCount(FULL_BUCKET_ID) > 0;
+								}
+							}, 4000);
+					} else {
+						if (Clicking.click("Use", Inventory.find(EMPTY_BUCKET_ID))) {
+							Timing.waitUptext("->", 4000);
+						}
+					}
+				}
+			} else {
+				if (sink.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+					Camera.turnToTile(sink);
+				} else {
+					Walking.blindWalkTo(sink.getPosition());
+				}
+			}
+		}
+	}
+
+	private void coolBars() {
+		RSObject dispenser = Get.getObject(BAR_DISPENSER_TILE);
+		if (dispenser != null) {
+			if (dispenser.isOnScreen()) {
+				String uptext = Game.getUptext();
+				if(uptext != null) {
+					if(uptext.contains("->")) {
+						if (dispenser.click("Use Bucket of water -> Bar dispenser"))
+							Timing.waitCondition(new Condition() {
+								@Override
+								public boolean active() {
+									return false; //check if bars cooled
+								}
+							}, 3000);
+					} else {
+						if (Clicking.click("Use", Inventory.find(FULL_BUCKET_ID))) {
+							Timing.waitUptext("Use Bucket of water ->", 4000);
+						}
+					}
+				}
+			} else {
+				if (dispenser.getPosition().distanceTo(Player.getRSPlayer()) <= 4) {
+					Camera.turnToTile(dispenser);
+				} else {
+					Walking.blindWalkTo(dispenser.getPosition());
+				}
+			}
+		}
+	}
 }
