4fa12e3cd44c33d99761e821bd70b2d41aa89329
Nolan <9oxsteelflux@gmail.com>
2014-04-21 14:42:04 -0400

    debugs

--- BlastFurnaceScript/src/scripts/blastfurnace/util/Statics.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/Statics.java
@@ -1,13 +1,18 @@
 package scripts.blastfurnace.util;
 
+import org.tribot.api2007.types.RSArea;
+import org.tribot.api2007.types.RSTile;
+
 /**
  * @author Starfox
  */
 public class Statics {
+    public static final RSArea BLAST_FURNACE_AREA = new RSArea(new RSTile(1935, 4974, 0), new RSTile(1957 , 4956, 0)); 
     public static String shoutCallerName = "";
     public static boolean startPumping = false;
     public static int startWorld;
     public static String jobName = "";
     public static boolean doJob = false;
     public static String bar = "";
+    public static int barCount = 0;
 }
