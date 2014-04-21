03f299221f80d9d2b371ea59cc7f54ac3e4fb1a5
Nolan <9oxsteelflux@gmail.com>
2014-04-21 01:09:55 -0400

    CC util methods

--- BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/RSUtil.java
@@ -1,8 +1,17 @@
 package scripts.blastfurnace.util;
 
+import java.awt.Color;
+import java.awt.Point;
 import org.tribot.api.Clicking;
+import org.tribot.api.Timing;
+import org.tribot.api.input.Keyboard;
+import org.tribot.api.types.generic.Condition;
 import org.tribot.api2007.Game;
+import org.tribot.api2007.GameTab;
+import org.tribot.api2007.Interfaces;
+import org.tribot.api2007.Screen;
 import org.tribot.api2007.types.RSGroundItem;
+import org.tribot.api2007.types.RSInterfaceChild;
 import org.tribot.api2007.types.RSNPC;
 import org.tribot.api2007.types.RSObject;
 import org.tribot.api2007.types.RSTile;
@@ -11,6 +20,7 @@ public class RSUtil {
 
     /**
      * Clicks the RSObject specified with the passed in option.
+     *
      * @param option The option to select.
      * @param entities RSObject(s) you want to click.
      * @return returns true if it successfully clicks the object.
@@ -25,6 +35,7 @@ public class RSUtil {
 
     /**
      * Clicks the RSNPC specified with the passed in option
+     *
      * @param option The option to select.
      * @param entities RSNPC(s) you want to click.
      * @return returns true if it successfully clicks the npc.
@@ -36,9 +47,10 @@ public class RSUtil {
         }
         return false;
     }
-    
+
     /**
      * Clicks the RSGroundItem specified with the passed in option
+     *
      * @param option The option to select.
      * @param entities RSGroundItem(s) you want to click.
      * @return returns true if it successfully clicks the RSGroundItem.
@@ -49,5 +61,73 @@ public class RSUtil {
             return destination != null && entities[0].getPosition().distanceTo(destination) <= 1;
         }
         return false;
+    }
+
+    /**
+     * Checks to see if you're in a cc.
+     *
+     * @return true if in a cc; false otherwise.
+     */
+    public static boolean isInCC() {
+        final RSInterfaceChild child = Interfaces.get(589, 1);
+        if (child != null) {
+            String text = child.getText();
+            return text != null && !text.contains("None");
+        }
+        return false;
+    }
+
+    /**
+     * Joins the specified cc.
+     *
+     * @param name The name of the cc.
+     * @return true if successful; false otherwise.
+     */
+    public static boolean joinCC(final String name) {
+        if (isInCC()) {
+            return true;
+        }
+        if (RSUtil.isEnterAmountUp()) {
+            Keyboard.typeSend(name);
+            return Timing.waitCondition(new Condition() {
+                @Override
+                public boolean active() {
+                    return isInCC();
+                }
+            }, 3000);
+        }
+        if (!GameTab.TABS.CLAN.isOpen()) {
+            Keyboard.pressFunctionKey(7);
+        }
+        if (GameTab.TABS.CLAN.isOpen()) {
+            final RSInterfaceChild button = Interfaces.get(589, 2);
+            if (button != null && button.click()) {
+                Timing.waitCondition(new Condition() {
+                    @Override
+                    public boolean active() {
+                        return RSUtil.isEnterAmountUp();
+                    }
+                }, 2500);
+                if (RSUtil.isEnterAmountUp()) {
+                    Keyboard.typeSend(name);
+                    return Timing.waitCondition(new Condition() {
+                        @Override
+                        public boolean active() {
+                            return isInCC();
+                        }
+                    }, 3000);
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks to see whether or not the enter amount menu is visible.
+     *
+     * @return true if it's visible; false otherwise.
+     */
+    public static boolean isEnterAmountUp() {
+        return Screen.getColorAt(new Point(260, 428)).equals(new Color(0, 0, 128));
     }
 }
