cf56389184537b58aa05b3cb858e63240725d1f8
Nolan <9oxsteelflux@gmail.com>
2014-04-21 14:42:10 -0400

    Util classes

---
+++ BlastFurnaceScript/src/scripts/blastfurnace/util/InventoryListener.java
@@ -0,0 +1,6 @@
+package scripts.blastfurnace.util;
+
+public interface InventoryListener {
+    public void inventoryItemAdded(int id, int count);
+    public void inventoryItemRemoved(int id, int count);
+}
