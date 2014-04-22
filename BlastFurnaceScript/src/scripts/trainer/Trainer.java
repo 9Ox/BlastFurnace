fbcf8514bc44463d5097578dbe8f849d2d3f6522
Nolan <9oxsteelflux@gmail.com>
2014-04-22 16:02:33 -0400

    i forget

--- BlastFurnaceScript/src/scripts/trainer/Trainer.java
+++ BlastFurnaceScript/src/scripts/trainer/Trainer.java
@@ -41,16 +41,15 @@ import scripts.blastfurnace.util.WorldHop;
 @ScriptManifest(name = "Smither Trainer", authors = "", category = "No category")
 public class Trainer extends Script implements MouseSplinePainting, MessageListening07 {
 
-
-	//START VARIABLE DECLARATION
-	Smithable smithable = Smithable.BRONZE_DAGGER;
-	private final RSArea BLAST_FURNACE_AREA = new RSArea(new RSTile(1935, 4974, 0), new RSTile(1957, 4956, 0));
-	private final int HAMMER_ID = 2347;
-	private final String TRADER_NAME = "9Ox";
-	private final RSTile LUMBRIDGE_TILE = new RSTile(3232,3230,0);
-	private final static int[] JUNK_ITEMS  = {1351,590,303,315,1925,131,2309,1265,1205,1277,1171,841,852,1931,882};
-	private int STARTING_WORLD; // take away the 3 in the world
-	//END VARIABLE DECLARATION
+    //START VARIABLE DECLARATION
+    Smithable smithable = Smithable.BRONZE_DAGGER;
+    private final RSArea BLAST_FURNACE_AREA = new RSArea(new RSTile(1935, 4974, 0), new RSTile(1957, 4956, 0));
+    private final int HAMMER_ID = 2347;
+    private final String TRADER_NAME = "9Ox";
+    private final RSTile LUMBRIDGE_TILE = new RSTile(3232, 3230, 0);
+    private final static int[] JUNK_ITEMS = {1351, 590, 303, 315, 1925, 131, 2309, 1265, 1205, 1277, 1171, 841, 852, 1931, 882};
+    private int STARTING_WORLD; // take away the 3 in the world
+    //END VARIABLE DECLARATION
 
     @Override
     public void run() {
@@ -58,39 +57,40 @@ public class Trainer extends Script implements MouseSplinePainting, MessageListe
         Mouse.setSpeed(150);
         ThreadSettings.get().setClickingAPIUseDynamic(true);
         while (!BLAST_FURNACE_AREA.contains(Player.getPosition())) {
-        	if(needsToTrade()) {
-				if(WorldHop.getWorld() != STARTING_WORLD) {
-					setLoginBotState(false);
-					if(WorldHop.switchWorld(STARTING_WORLD)) {
-						setLoginBotState(true);
-						Timing.waitCondition(new Condition() {
-							@Override
-							public boolean active() {
-								// TODO Auto-generated method stub
-								return Game.getGameState() == 30;
-							}
-						}, 10000);
+            if (needsToTrade()) {
+                if (WorldHop.getWorld() != STARTING_WORLD) {
+                    setLoginBotState(false);
+                    if (WorldHop.switchWorld(STARTING_WORLD)) {
+                        setLoginBotState(true);
+                        Timing.waitCondition(new Condition() {
+                            @Override
+                            public boolean active() {
+                                // TODO Auto-generated method stub
+                                return Game.getGameState() == 30;
+                            }
+                        }, 10000);
 
-					}
-				} else if(Trading.getFirstTradeInterface() != null) {
-					Trading.accept(1);
-				} else if(Trading.getSecondTradeInterface() != null) {
-					Trading.accept(2);
-				} else {
-					Inventory.drop(JUNK_ITEMS);
-				}
-				
-			} else if(!isInVarrock()) {
-				RSItem[] tab = Inventory.find(8007);
-				if(tab.length > 0 && tab[0].click())
-					Timing.waitCondition(new Condition() {
-						@Override
-						public boolean active() {
-							return isInVarrock();
-						}
-					}, 10500);
-				
-			} else if(SKILLS.SMITHING.getActualLevel() < 60) {
+                    }
+                } else if (Trading.getFirstTradeInterface() != null) {
+                    Trading.accept(1);
+                } else if (Trading.getSecondTradeInterface() != null) {
+                    Trading.accept(2);
+                } else {
+                    Inventory.drop(JUNK_ITEMS);
+                }
+
+            } else if (!isInVarrock()) {
+                RSItem[] tab = Inventory.find(8007);
+                if (tab.length > 0 && tab[0].click()) {
+                    Timing.waitCondition(new Condition() {
+                        @Override
+                        public boolean active() {
+                            return isInVarrock();
+                        }
+                    }, 10500);
+                }
+
+            } else if (SKILLS.SMITHING.getActualLevel() < 60) {
                 if (Camera.getCameraRotation() < 170 || Camera.getCameraRotation() > 185) {
                     Camera.setCameraRotation(General.random(170, 185));
                 }
@@ -155,6 +155,7 @@ public class Trainer extends Script implements MouseSplinePainting, MessageListe
                         }
                     } else {
                         if (smithable.getComponent() == null) {
+                            Inventory.open();
                             if (Clicking.click("Use", Inventory.find(smithable.getBarId()))) {
                                 sleep(150);
                                 if (Clicking.click("Anvil", Objects.findNearest(20, "Anvil"))) {
@@ -210,32 +211,33 @@ public class Trainer extends Script implements MouseSplinePainting, MessageListe
     }
 
     private boolean needsToTrade() {
-		return Player.getPosition().distanceTo(LUMBRIDGE_TILE) <= 50 && Inventory.find(8007).length == 0;
-	}
-    
-	private String getFormattedName(String s) {
-		return s.replaceAll(" ", " ");
-	}
-	
-	private boolean clickPlayer(RSPlayer player, String option) {
+        return Player.getPosition().distanceTo(LUMBRIDGE_TILE) <= 50 && Inventory.find(8007).length == 0;
+    }
 
-		if (player != null) {
-			if(player.isOnScreen()) {
-				if(player.hover())
-					Mouse.click(3);
-				if (ChooseOption.isOpen()) {
-					if (ChooseOption.select(option)) {
-						return true;
-					}
-				}
-			} else {
-				Walking.walkTo(player);
-				Camera.turnToTile(player);
-			}
-		}
-		return false;
-	}
-	
+    private String getFormattedName(String s) {
+        return s.replaceAll(" ", " ");
+    }
+
+    private boolean clickPlayer(RSPlayer player, String option) {
+
+        if (player != null) {
+            if (player.isOnScreen()) {
+                if (player.hover()) {
+                    Mouse.click(3);
+                }
+                if (ChooseOption.isOpen()) {
+                    if (ChooseOption.select(option)) {
+                        return true;
+                    }
+                }
+            } else {
+                Walking.walkTo(player);
+                Camera.turnToTile(player);
+            }
+        }
+        return false;
+    }
+
     private boolean shouldWalk(RSTile dest) {
         if (dest == null) {
             return false;
@@ -253,7 +255,7 @@ public class Trainer extends Script implements MouseSplinePainting, MessageListe
     private boolean inventoryContainsOnly(int... ids) {
         return Inventory.getAll().length <= Inventory.find(ids).length;
     }
-    
+
     private void switchSmithable() {
         int level = Skills.SKILLS.SMITHING.getActualLevel();
         if (level >= 48) {
@@ -273,69 +275,68 @@ public class Trainer extends Script implements MouseSplinePainting, MessageListe
         }
     }
 
+    @Override
+    public void clanMessageReceived(String arg0, String arg1) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void clanMessageReceived(String arg0, String arg1) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void personalMessageReceived(String arg0, String arg1) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void personalMessageReceived(String arg0, String arg1) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void playerMessageReceived(String arg0, String arg1) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void playerMessageReceived(String arg0, String arg1) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void serverMessageReceived(String arg0) {
+        if (arg0.contains("Accepted")) {
+            setLoginBotState(false);
+            if (WorldHop.switchWorld(WorldHop.getRandomWorld())) {
+                setLoginBotState(true);
+                Timing.waitCondition(new Condition() {
+                    @Override
+                    public boolean active() {
+                        // TODO Auto-generated method stub
+                        return Game.getGameState() == 30;
+                    }
+                }, 10000);
+            }
+        }
+        // TODO Auto-generated method stub
 
-	@Override
-	public void serverMessageReceived(String arg0) {
-		if(arg0.contains("Accepted")) {
-			setLoginBotState(false);
-			if(WorldHop.switchWorld(WorldHop.getRandomWorld())) {
-				setLoginBotState(true);
-				Timing.waitCondition(new Condition() {
-					@Override
-					public boolean active() {
-						// TODO Auto-generated method stub
-						return Game.getGameState() == 30;
-					}
-				}, 10000);
-			}
-		}
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void tradeRequestReceived(String arg0) {
+        if (Inventory.find(JUNK_ITEMS).length > 0) {
+            return;
+        }
+        if (getFormattedName(arg0).equalsIgnoreCase(TRADER_NAME)) {
+            RSPlayer[] players = Players.findNearest(arg0);
+            if (players.length > 0) {
+                for (int i = 0; i < 10; i++) {
+                    if (clickPlayer(players[0], "Trade with " + players[0].getName())) {
+                        Timing.waitCondition(new Condition() {
+                            @Override
+                            public boolean active() {
+                                // TODO Auto-generated method stub
+                                return Trading.getFirstTradeInterface() != null;
+                            }
+                        }, 4000);
+                        break;
+                    }
+                }
+            }
+        }
+        // TODO Auto-generated method stub
 
-	@Override
-	public void tradeRequestReceived(String arg0) {
-		if(Inventory.find(JUNK_ITEMS).length > 0)
-			return;
-		if(getFormattedName(arg0).equalsIgnoreCase(TRADER_NAME)) {
-			RSPlayer[] players = Players.findNearest(arg0);
-			if(players.length > 0) {
-				for(int i = 0; i < 10; i++) {
-					if(clickPlayer(players[0], "Trade with " + players[0].getName())) {
-						Timing.waitCondition(new Condition() {
-							@Override
-							public boolean active() {
-								// TODO Auto-generated method stub
-								return Trading.getFirstTradeInterface() != null;
-							}
-						}, 4000);
-						break;
-					}
-				}
-			}
-		}
-		// TODO Auto-generated method stub
-
-	}
-
+    }
 
     @Override
     public void paintMouseSpline(Graphics g, ArrayList<Point> al) {
